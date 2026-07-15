43af11ac9524d90891624e53519f5676de98fecc
Neall Dexter <neall.t.dexter@gmail.com>
2026-07-15 13:31:16 +0100

    API for login and registration added as well as authentication

--- backend/settings.py
+++ backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'base.authentication.CookiesJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
