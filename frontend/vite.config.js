f0fe354ac22fb65c45598c80a77fba8ade8c66ec
Neall Dexter <neall.t.dexter@gmail.com>
2026-07-31 18:34:44 +0100

    allowed hosts added to config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   build: {
-    outDir: "dist",       
-    emptyOutDir: true      
+    outDir: "dist",
+    emptyOutDir: true
+  },
+  preview: {
+    allowedHosts: ["vitality-vault-frontend-c226e07c9727.herokuapp.com"]
   }
 });
