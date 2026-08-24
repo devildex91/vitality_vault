0f17692ca3d3635fa19af829b5d370599f8cbe7f
Neall Dexter <neall.t.dexter@gmail.com>
2026-08-24 19:35:27 +0100

    Editplan delete exercise/day/workout buttons and frontend functionality added

--- frontend/src/pages/workout-page/Edit.jsx
+++ frontend/src/pages/workout-page/Edit.jsx
@@ -11,18 +11,11 @@ export default function EditPlan({}) {
   const targetExercises = selectedDay?.exercises;
   const [editedExercises, setEditedExercises] = useState({});
   
- function updateExercise() {
-  const updatedExercises = targetExercises.map((exercise, index) => {
-    return {
-      ...exercise,
-      ...(editedExercises[index] || {}),
-    };
-  });
-
-  setSelectedDay({
-    ...selectedDay,
-    exercises: updatedExercises,
-  });
+ function saveWorkout(e) {
+ e.preventDefault()
+ if(!selectedWorkout){
+  alert("You have selected no options at all. Please try again")
+ }
 }
 console.log(selectedDay?.exercises)
 
@@ -37,7 +30,7 @@ console.log(selectedDay?.exercises)
   }
 
   function findDay(targetDay) {
-    const foundDay = selectedWorkout.days?.find(
+    const foundDay = selectedWorkout?.days?.find(
       (workout) => workout.day === targetDay,
     );
 
@@ -46,12 +39,23 @@ console.log(selectedDay?.exercises)
     }
   }
   function findExercise(targetExercise) {
+    if(targetExercise === "Add Exercise" || targetExercise === ""){
+      setSelectedExercise(null)
+      setNewExercise({exercise:"", sets:"", reps:""});
+      return;
+    }
+
     const foundExercise = selectedDay.exercises?.find(
       (exercise) => exercise.exercise === targetExercise,
     );
     if(foundExercise) {
       setSelectedExercise(foundExercise);
-    }
+       setNewExercise({
+        exercise: foundExercise.exercise,
+        sets: foundExercise.sets,
+        reps: foundExercise.reps,
+         });
+    } else setSelectedExercise(null)
   }
  
 
@@ -72,17 +76,113 @@ console.log(selectedDay?.exercises)
       })
     : [];
 
-const seeWorkout = (e) => {
-  e.preventDefault() 
-  setSelectedWorkout((workout) => {
+function updateWorkout() { 
+ if (!selectedWorkout || !selectedDay){
+  alert("You have made no selections please fill out the options below to continue")
+  return
+ }
 
-  })
-}
-console.log(selectedWorkout)
+ setSelectedWorkout((prevWorkout) => {
+  if (!prevWorkout) return prevWorkout;
+
+  return{
+    ...prevWorkout,
+    days: prevWorkout.days.map((dayObj) => {
+      if (dayObj?.day === selectedDay?.day){
+        //updating existing 
+        if(selectedExercise) {return {
+           ...dayObj,
+           exercises: dayObj.exercises.map((ex) => {
+            if (ex.exercise === selectedExercise.exercise) {
+              return{
+                ...ex,
+                exercise:newExercise?.exercise,
+                sets:Number(newExercise?.sets) || 0,
+                reps:Number(newExercise?.reps) || 0,
+              };
+            } 
+            return ex;
+            }), 
+           };
+          }
+          //Adding new exercise
+          else {
+            const brandNewExerciseItem = {
+              id: Date.now().toString(),
+              exercise:newExercise.exercise,
+              sets:Number(newExercise?.sets) || 0,
+                reps:Number(newExercise?.reps) || 0,
+            };
+            return {
+              ...dayObj,
+              exercises: [...dayObj.exercises, brandNewExerciseItem],
+            };
+          }
+        }
+        return dayObj;
+             }),
+      };
+    });
+    //resets fields after submitting
+    setSelectedExercise(null);
+    setNewExercise({ exercise: "", sets: "", reps: "" });
+  }
+
+function deleteExercise() {
+  if(!selectedExercise){
+    alert("Please select an exercise to continue")
+  } else {
+  setSelectedWorkout((prevWorkout) => {
+    return{
+      ...prevWorkout,
+      days: prevWorkout.days.map((days) => {
+        if(days.day === selectedDay.day) {
+          return {
+            ...days,
+            exercises: days.exercises.filter(
+               (exercise) => exercise.exercise !== selectedExercise.exercise)
+            };
+          }
+          return days;
+          })
+      };
+    });
+  }}
+
+     
+function deleteDay(){
+  if (!selectedDay){
+    alert("You have not selected a day to delete. Please select a day and try again.")
+  } else {
+ setSelectedWorkout((prevWorkout) => {
+  return {
+    ...prevWorkout, 
+    days: prevWorkout.days.map((days) => {
+      if(days.day === selectedDay.day) {
+        return {
+          ...days,
+          exercises: []
+        };
+      }
+      return days;
+    })
+  }
+ })
+  }
+ } 
+
+function deleteWorkout(){
+  if (!selectedWorkout){
+    alert("Please Select a Workout to continue")
+  } else {
+setSelectedWorkout(null)
+}}
+
+console.log(selectedDay)
 console.log(sortedPlan)
   return (
     <div className="card lg:card-side bg-accent text-secondary-content shadow-sm w-full overflow-x-auto">
-     <form onSubmit ={seeWorkout}>
+     <form onSubmit ={saveWorkout}>
       {/*Workout Select */}
       <select
         className="select select-accent"
@@ -121,11 +221,12 @@ console.log(sortedPlan)
       {/*Current exercise select  */}
       <select
        className = "select select-accent"
-       value = {selectedExercise?.exercise}
+       value = {selectedExercise?.exercise || ""}
        disabled = {!selectedDay}
        onChange={(e) => findExercise(e.target.value)}
       >
-        <option value = "">--Select Exercise to change--</option>
+        <option value = "">--Select Exercise / Add new--</option>
+        <option value = "Add Exercise">Add New</option>
         {selectedDay?.exercises?.map((exercise) => {
           return (
             <option key = {exercise?.id} value={exercise?.exercise}>{exercise?.exercise}</option>
@@ -137,15 +238,15 @@ console.log(sortedPlan)
       <select
       className = "select select-accent"
       value = {newExercise?.exercise}
-      disabled = {!selectedExercise}
+      disabled = {!selectedDay}
       onChange = {(e) => setNewExercise((exercise) =>({
-        exercise: e.target.value,
         ...exercise,
+        exercise: e.target.value,
       }) )}>
         <option value="">--Choose new exercise--</option>
         {exerciseData.map((exerciseObj) => {
                     return (
-                      <option key={exerciseObj.id} value={exerciseObj.id}>
+                      <option key={exerciseObj.id} value={exerciseObj.exercise}>
                         {exerciseObj.name}
                       </option>
                     );
@@ -182,8 +283,11 @@ onChange={(e) => setNewExercise((exercise) => ({
   ))}
 
 </select>
-      
-      <button type = "submit">See updated Workout</button>
+      <button className="btn btn-soft text-accent bg-base-100 block" type = "button" onClick = {updateWorkout}>Update Workout</button>
+      <button className="btn btn-soft text-accent bg-base-100 block" type = "button" onClick = {deleteExercise}>Delete Exercise</button>
+      <button className="btn btn-soft text-accent bg-base-100 block" type = "button" onClick = {deleteDay}>Delete day</button>
+      <button className="btn btn-soft text-accent bg-base-100 block" type = "button" onClick = {deleteWorkout}>Delete Workout</button>
+      <button  className="btn btn-soft text-accent bg-base-100 block" type = "submit">Save Workout</button>
          </form>
         <div id = "workoutDisplay"
           className = "card lg:card-side bg-accent text-secondary-content shadow-sm w-full overflow-x-auto">
